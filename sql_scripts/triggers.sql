fb80feeb3cc13a6339d6317cded7b2ce4f164147
Python Wizard <max.python.wizard@gmail.com>
2022-05-31 01:09:31 +0200

    added triggers: before_follow (checking that person is not following themselves) and logging follows and unfollow (log_after_follow and log_after_unfollow) in sql_scripts/triggers.sql

---
+++ sql_scripts/triggers.sql
@@ -0,0 +1,47 @@
+# trigger checks for two things and prevents insert if either of the two conditions are met:
+# if one user is trying to follow oneself
+# if user follow source is already following the target
+drop trigger if exists before_follow;
+delimiter $
+create trigger before_follow before insert on follow
+for each row
+begin
+    if new.follow_source = new.follow_target
+    then
+        signal sqlstate '45000' set message_text = "One cannot follow oneself, follow  and sourcetarget have to be different";
+    end if ;
+
+    if (select count(*) from follow where follow_source = new.follow_source and follow_target = new.follow_target) > 0
+    then
+        signal sqlstate '45000' set message_text = "This user is already following that user";
+    end if ;
+end $
+delimiter ;
+
+drop trigger if exists after_follow;
+delimiter $
+create trigger after_follow before insert on follow
+for each row
+begin
+
+    insert into logs_follow (follow_source, follow_target, change_fol, time) value
+        (new.follow_source, new.follow_target, 'follow', now());
+
+end $
+delimiter ;
+
+select * from logs_follow;
+
+drop trigger if exists after_unfollow;
+delimiter $
+create trigger after_unfollow after delete on follow
+for each row
+begin
+
+    insert into logs_follow (follow_source, follow_target, change_fol, time) value
+        (old.follow_source, old.follow_target, 'unfollow', now());
+
+end $
+delimiter ;
+
+select * from logs_follow;
