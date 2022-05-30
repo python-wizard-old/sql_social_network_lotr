2b93221f89f52a61d65079ce2aeaefe51a23f8f4
Python Wizard <max.python.wizard@gmail.com>
2022-05-31 01:11:13 +0200

    adding follows in sql_scripts/add_follows.sql

---
+++ sql_scripts/add_follows.sql
@@ -0,0 +1,7 @@
+call follow('gandalf', 'aragorn');
+call follow('gandalf', 'gollum');
+call follow('gandalf', 'sauron');
+call follow('gandalf', 'gandalf');
+call follow('bilbo', 'gandalf');
+call follow('gandalf', 'gandalf');
+call follow('sauron', 'gandalf');
