58759898c2f61a5e64a6fae572edae2ca3f7cbe7
Python Wizard <max.python.wizard@gmail.com>
2022-06-01 00:44:14 +0200

    added script adding comments in sql_scripts/add_comments.sql

---
+++ sql_scripts/add_comments.sql
@@ -0,0 +1,69 @@
+# Bilbo
+call comment('thorin', 'bilbo-visited-dwarfes', "You seem like a very honorable Hobbit. I hope to see you soon.");
+call comment('bombur', 'bilbo-visited-dwarfes', "I was on a diet, but because of you I broke that.");
+call comment('gandalf', 'bilbo-visited-dwarfes', "Like!");
+
+call comment('gandalf', 'bilbo-beorn', "I'm glad you found him.");
+call comment('beorn', 'bilbo-beorn', "Like!");
+
+call comment('gollum', 'bilbo-beorn', "You evil hobbit, return the ring to the master!");
+call comment('gollum', 'bilbo-beorn', "Where is my precious?");
+
+call comment('gandalf', 'bilbo-beorn', "You've done the right thing, my friend!");
+
+# Gollum
+# "I defeated Deagor to get the ring."
+
+call comment('gandalf', 'gollum-defeated-deagor', "Be carefoul before you wear the ring, it may have special powers.");
+
+# aragorn
+
+
+call comment('thorin', 'aragorn-fellowship', "Like!");
+call comment('bombur', 'aragorn-fellowship', "Like!");
+call comment('gandalf', 'aragorn-fellowship', "Like!");
+call comment('bilbo', 'aragorn-fellowship', "Like!");
+call comment('balin', 'aragorn-fellowship', "Like!");
+call comment('elrond', 'aragorn-fellowship', "Like!");
+call comment('bohomir', 'aragorn-fellowship', "Like!");
+call comment('galadriel', 'aragorn-fellowship', "Like!");
+
+call comment('thorin', 'aragorn-king', "Like!");
+call comment('bombur', 'aragorn-king', "Like!");
+call comment('gandalf', 'aragorn-king', "Like!");
+call comment('bilbo', 'aragorn-king', "Like!");
+call comment('balin', 'aragorn-king', "Like!");
+call comment('elrond', 'aragorn-king', "Like!");
+call comment('bohomir', 'aragorn-king', "Like!");
+call comment('galadriel', 'aragorn-king', "Like!");
+
+# Sauron
+
+call comment('thorin', 'sauron-defeat-all', "Hate!");
+call comment('gandalf', 'sauron-defeat-all', "Hate!");
+call comment('bilbo', 'sauron-defeat-all', "Hate!");
+call comment('elrond', 'sauron-defeat-all', "Hate!");
+call comment('bohomir', 'sauron-defeat-all', "Hate!");
+call comment('galadriel', 'sauron-defeat-all', "Hate!");
+
+
+# Gandalf
+# "I visited Beorn."
+
+call comment('beorn', 'gandalf-visited-beorn', "I was glad to host you!");
+
+# Frodo
+
+call comment('elrond', 'frodo-survived-blackrider', "I'm glad you'll safe with us at Rivendell!");
+
+call comment('gandalf', 'frodo-met-elrond', "Like!");
+call comment('elrond', 'frodo-met-elrond', "Like!");
+call comment('galadriel', 'frodo-met-elrond', "Like!");
+
+# Bombur - Dwarf
+
+call comment('bilbo', 'bombur-diet-bilbo', "You were on such a diet that you've eated all my supplies...");
+
+# Galion
+
+call comment('bilbo', 'galion-bilbo-escaped', "Catch me if you can :)");
